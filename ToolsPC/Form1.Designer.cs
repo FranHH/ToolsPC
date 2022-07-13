face6e5cf50d0aecffd10bb10442776fc79b3b61
FranDev <fjperez@iccaweb.com>
2022-07-13 10:11:46 +0200

    Commit principal

---
+++ ToolsPC/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace ToolsPC
+{
+    partial class ToolsPC
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToolsPC));
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbSegundos = new System.Windows.Forms.Label();
+            this.lbContador = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cbApagar = new System.Windows.Forms.CheckBox();
+            this.cbReiniciar = new System.Windows.Forms.CheckBox();
+            this.cbHibernar = new System.Windows.Forms.CheckBox();
+            this.cbCerrarSesion = new System.Windows.Forms.CheckBox();
+            this.dtHoraEjecucion = new System.Windows.Forms.DateTimePicker();
+            this.pbEstado = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(279, 131);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.Aceptar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lbSegundos);
+            this.panel1.Controls.Add(this.lbContador);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.dtHoraEjecucion);
+            this.panel1.Controls.Add(this.btnAceptar);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 168);
+            this.panel1.TabIndex = 1;
+            // 
+            // lbSegundos
+            // 
+            this.lbSegundos.AutoSize = true;
+            this.lbSegundos.Location = new System.Drawing.Point(310, 48);
+            this.lbSegundos.Name = "lbSegundos";
+            this.lbSegundos.Size = new System.Drawing.Size(53, 13);
+            this.lbSegundos.TabIndex = 11;
+            this.lbSegundos.Text = "segundos";
+            this.lbSegundos.Visible = false;
+            // 
+            // lbContador
+            // 
+            this.lbContador.AutoSize = true;
+            this.lbContador.Location = new System.Drawing.Point(276, 48);
+            this.lbContador.Name = "lbContador";
+            this.lbContador.Size = new System.Drawing.Size(37, 13);
+            this.lbContador.TabIndex = 10;
+            this.lbContador.Text = "00000";
+            this.lbContador.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hora de ejecución";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.cbApagar);
+            this.panel2.Controls.Add(this.cbReiniciar);
+            this.panel2.Controls.Add(this.cbHibernar);
+            this.panel2.Controls.Add(this.cbCerrarSesion);
+            this.panel2.Location = new System.Drawing.Point(27, 48);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(202, 109);
+            this.panel2.TabIndex = 9;
+            // 
+            // cbApagar
+            // 
+            this.cbApagar.AutoSize = true;
+            this.cbApagar.Location = new System.Drawing.Point(70, 12);
+            this.cbApagar.Name = "cbApagar";
+            this.cbApagar.Size = new System.Drawing.Size(60, 17);
+            this.cbApagar.TabIndex = 4;
+            this.cbApagar.Text = "Apagar";
+            this.cbApagar.UseVisualStyleBackColor = true;
+            this.cbApagar.CheckedChanged += new System.EventHandler(this.cbApagar_CheckedChanged);
+            // 
+            // cbReiniciar
+            // 
+            this.cbReiniciar.AutoSize = true;
+            this.cbReiniciar.Location = new System.Drawing.Point(70, 35);
+            this.cbReiniciar.Name = "cbReiniciar";
+            this.cbReiniciar.Size = new System.Drawing.Size(67, 17);
+            this.cbReiniciar.TabIndex = 5;
+            this.cbReiniciar.Text = "Reiniciar";
+            this.cbReiniciar.UseVisualStyleBackColor = true;
+            this.cbReiniciar.CheckedChanged += new System.EventHandler(this.cbReiniciar_CheckedChanged);
+            // 
+            // cbHibernar
+            // 
+            this.cbHibernar.AutoSize = true;
+            this.cbHibernar.Location = new System.Drawing.Point(70, 81);
+            this.cbHibernar.Name = "cbHibernar";
+            this.cbHibernar.Size = new System.Drawing.Size(66, 17);
+            this.cbHibernar.TabIndex = 7;
+            this.cbHibernar.Text = "Hibernar";
+            this.cbHibernar.UseVisualStyleBackColor = true;
+            this.cbHibernar.CheckedChanged += new System.EventHandler(this.cbHibernar_CheckedChanged);
+            // 
+            // cbCerrarSesion
+            // 
+            this.cbCerrarSesion.AutoSize = true;
+            this.cbCerrarSesion.Location = new System.Drawing.Point(70, 58);
+            this.cbCerrarSesion.Name = "cbCerrarSesion";
+            this.cbCerrarSesion.Size = new System.Drawing.Size(87, 17);
+            this.cbCerrarSesion.TabIndex = 6;
+            this.cbCerrarSesion.Text = "Cerrar sesión";
+            this.cbCerrarSesion.UseVisualStyleBackColor = true;
+            this.cbCerrarSesion.CheckedChanged += new System.EventHandler(this.cbCerrarSesion_CheckedChanged);
+            // 
+            // dtHoraEjecucion
+            // 
+            this.dtHoraEjecucion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dtHoraEjecucion.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtHoraEjecucion.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dtHoraEjecucion.Location = new System.Drawing.Point(27, 22);
+            this.dtHoraEjecucion.Name = "dtHoraEjecucion";
+            this.dtHoraEjecucion.ShowUpDown = true;
+            this.dtHoraEjecucion.Size = new System.Drawing.Size(102, 20);
+            this.dtHoraEjecucion.TabIndex = 8;
+            this.dtHoraEjecucion.Value = new System.DateTime(2022, 7, 12, 0, 0, 0, 0);
+            // 
+            // pbEstado
+            // 
+            this.pbEstado.BackColor = System.Drawing.Color.SlateGray;
+            this.pbEstado.Location = new System.Drawing.Point(-13, 1);
+            this.pbEstado.Name = "pbEstado";
+            this.pbEstado.Size = new System.Drawing.Size(436, 10);
+            this.pbEstado.TabIndex = 10;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // ToolsPC
+            // 
+            this.AccessibleDescription = "Herramientas para PC";
+            this.AccessibleName = "ToolsPC";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(418, 181);
+            this.Controls.Add(this.pbEstado);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ToolsPC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ToolsPC";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dtHoraEjecucion;
+        private System.Windows.Forms.CheckBox cbHibernar;
+        private System.Windows.Forms.CheckBox cbCerrarSesion;
+        private System.Windows.Forms.CheckBox cbReiniciar;
+        private System.Windows.Forms.CheckBox cbApagar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ProgressBar pbEstado;
+        private System.Windows.Forms.Label lbContador;
+        private System.Windows.Forms.Label lbSegundos;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ToolsPC/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToolsPC
+{
+    public partial class ToolsPC : Form
+    {
+        private string info = string.Empty;
+        private System.Windows.Forms.Timer timer1;
+        private int tiempo;
+        private bool check = false;
+        public ToolsPC()
+        {
+            InitializeComponent();
+        }
+
+        private void Aceptar_Click(object sender, EventArgs e)
+        {
+            Principal();
+        }
+
+        private void Principal()
+        {
+            try
+            {
+                //ProgressBar Inicio 5%
+                pbEstado.Value = 5;
+                //Calculo el tiempo de ejecucion para la cuenta atras
+                List<int> tEjecucion = CalcularHora(dtHoraEjecucion.Value.Hour, dtHoraEjecucion.Value.Minute, dtHoraEjecucion.Value.Second);
+                //Convierto el tiempo a segundos
+                tiempo = ConversionHoras(tEjecucion[0], tEjecucion[1], tEjecucion[2]);
+                //int tiempo = ConversionHoras(dtHoraEjecucion.Value.Hour, dtHoraEjecucion.Value.Minute, dtHoraEjecucion.Value.Second);
+                string text = "Tarea programada correctamente.";
+                string command = string.Empty;
+                //ProgressBar 55%
+                pbEstado.Value += 35;
+
+                if (cbApagar.Checked)
+                {
+                    //ProgressBar 65%
+                    pbEstado.Value += 10;
+                    //Obtengo el comando
+                    command = "shutdown -s -t " + tiempo.ToString();
+                    //ProgressBar 80%
+                    pbEstado.Value += 15;
+                    //Muestro el contador
+                    lbContador.Visible = true;
+                    lbSegundos.Visible = true;
+
+                    //Creo un evento para la cuenta atrás
+                    timer1 = new System.Windows.Forms.Timer();
+                    timer1.Tick += new EventHandler(Contador);
+                    timer1.Interval = 1000;
+                    timer1.Start();
+                    check = true;
+
+                }
+                else if (cbReiniciar.Checked)
+                {
+                    //Reiniciar pc
+                    command = "shutdown /r";
+                    check = true;
+                }
+                else if (cbCerrarSesion.Checked)
+                {
+                    //Cerrar sesion pc
+                    command = "shutdown -L";
+                    check = true;
+                }
+                else if (cbHibernar.Checked)
+                {
+                    //HibernarPC
+                    command = "shutdown -h";
+                    check = true;
+                }
+                else
+                {
+                    //ProgressBar final 88%
+                    pbEstado.Value = 88;
+                    text = "Debes seleccionar al menos una opción";
+                    //Muestro mensaje de proceso no finalizado
+                    MessageBox.Show(text);
+                    //ProgressBar 0%
+                    pbEstado.Value = 0;
+                }
+
+                //Si todo está correcto, se inicia el proceso
+                if (check)
+                {
+                    this.backgroundWorker1.RunWorkerAsync(2000);
+                    //ProgressBar 100%
+                    pbEstado.Value += 20;
+                    //Muestro mensaje de ejecutado con exito
+                    MessageBox.Show(text);
+                    //ProgressBar final 0%
+                    pbEstado.Value = 0;
+                    //Inicio el comando
+                    Process.Start("cmd.exe", command);
+                    Application.Exit();
+                }
+            }
+            catch (Exception ex)
+            {
+                //ProgressBar final 88%
+                pbEstado.Value = 88;
+                //Obtengo el detalle del fallo
+                this.info = DateTime.Now.ToString() + " : " + ex.Message.ToString();
+                //Muestro mensaje del fallo
+                MessageBox.Show("Ha ocurrido un problema --> " + info);
+                //ProgressBar 0%
+                pbEstado.Value = 0;
+            }
+        }
+
+        #region Metodos auxiliares
+        /// <summary>
+        /// Evento para cuenta atrás.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Contador(object sender, EventArgs e)
+        {
+
+            if (tiempo == 0)
+            {
+                timer1.Stop();
+
+            }
+            else if (tiempo > 0)
+            {
+                tiempo--;
+                lbContador.Text = tiempo.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Metodo para calcular la hora exacta de ejecución
+        /// </summary>
+        /// <param name="hora"></param>
+        /// <param name="min"></param>
+        /// <param name="seg"></param>
+        /// <returns></returns>
+        private List<int> CalcularHora(int hora, int min, int seg)
+        {
+            List<int> tiempo = new List<int>();
+
+            int hActual = DateTime.Now.Hour;
+            int mActual = DateTime.Now.Minute;
+            int sActual = DateTime.Now.Second;
+
+            tiempo.Add(hora - hActual);
+            tiempo.Add(min - mActual);
+            tiempo.Add(seg - sActual);
+
+            if (tiempo[0] < 0 || tiempo[1] < 0 || tiempo[2] < 0)
+            {
+                //invertir tiempo
+                tiempo[0] = Math.Abs(tiempo[0]);
+                tiempo[1] = Math.Abs(tiempo[1]);
+                tiempo[2] = Math.Abs(tiempo[2]);
+                //Calcular hora. 
+                tiempo[0] = 23 - tiempo[0];
+                tiempo[1] = 59 - tiempo[1];
+                tiempo[2] = 59 - tiempo[2];
+               
+            }
+
+            return tiempo ;
+        }
+
+        /// <summary>
+        /// Metodo para convertir el tiempo a segundos 
+        /// </summary>
+        /// <param name="horas"></param>
+        /// <param name="minutos"></param>
+        /// <param name="segundos"></param>
+        /// <returns></returns>
+        private int ConversionHoras(int horas, int minutos, int segundos)
+        {
+            //ProgressBar 10%
+            pbEstado.Value += 5;
+            int resultado = (horas * 3600) + (minutos * 60) + segundos;
+            //ProgressBar 20%
+            pbEstado.Value += 10;
+            return resultado;
+        }
+        #endregion
+
+        #region CB CheckedChanged
+        private void cbApagar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbApagar.Checked)
+            {
+                cbReiniciar.Enabled = false;
+                cbHibernar.Enabled = false;
+                cbCerrarSesion.Enabled = false;
+            }
+            else
+            {
+                cbReiniciar.Enabled = true;
+                cbHibernar.Enabled = true;
+                cbCerrarSesion.Enabled = true;                
+            }
+        }
+
+        private void cbReiniciar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbReiniciar.Checked)
+            {
+                cbApagar.Enabled = false;
+                cbHibernar.Enabled = false;
+                cbCerrarSesion.Enabled = false;
+            }
+            else
+            {
+                cbApagar.Enabled = true;
+                cbHibernar.Enabled = true;
+                cbCerrarSesion.Enabled = true;
+            }
+        }
+
+        private void cbCerrarSesion_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbCerrarSesion.Checked)
+            {
+                cbReiniciar.Enabled = false;
+                cbHibernar.Enabled = false;
+                cbApagar.Enabled = false;
+            }
+            else
+            {
+                cbReiniciar.Enabled = true;
+                cbHibernar.Enabled = true;
+                cbApagar.Enabled = true;
+            }
+        }
+
+      
+
+        private void cbHibernar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbHibernar.Checked)
+            {
+                cbReiniciar.Enabled = false;
+                cbApagar.Enabled = false;
+                cbCerrarSesion.Enabled = false;
+            }
+            else
+            {
+                cbReiniciar.Enabled = true;
+                cbApagar.Enabled = true;
+                cbCerrarSesion.Enabled = true;
+            }
+        }
+
+        #endregion
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+       
+            BackgroundWorker bw = sender as BackgroundWorker;
+
+            // Extract the argument.
+            int arg = (int)e.Argument;
+
+            // Start the time-consuming operation.
+            e.Result = TimeConsumingOperation(bw, arg);
+
+            // If the operation was canceled by the user,
+            // set the DoWorkEventArgs.Cancel property to true.
+            if (bw.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+        }
+        private int TimeConsumingOperation(BackgroundWorker bw, int sleepPeriod)
+        {
+            int result = 0;
+
+            Random rand = new Random();
+
+            while (!bw.CancellationPending)
+            {
+                bool exit = false;
+
+                switch (rand.Next(3))
+                {
+                    // Raise an exception.
+                    case 0:
+                        {
+                            throw new Exception("An error condition occurred.");
+                            break;
+                        }
+
+                    // Sleep for the number of milliseconds
+                    // specified by the sleepPeriod parameter.
+                    case 1:
+                        {
+                            Thread.Sleep(sleepPeriod);
+                            break;
+                        }
+
+                    // Exit and return normally.
+                    case 2:
+                        {
+                            result = 23;
+                            exit = true;
+                            break;
+                        }
+
+                    default:
+                        {
+                            break;
+                        }
+                }
+
+                if (exit)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                // The user canceled the operation.
+                MessageBox.Show("Operation was canceled");
+            }
+            else if (e.Error != null)
+            {
+                // There was an error during the operation.
+                string msg = String.Format("An error occurred: {0}", e.Error.Message);
+                MessageBox.Show(msg);
+            }
+            else
+            {
+                // The operation completed normally.
+                string msg = String.Format("Result = {0}", e.Result);
+                MessageBox.Show(msg);
+            }
+        }
+
+   
+    }
+}
